da45079e2dccaf1c2282744da798312f94d320cd
Dhruva Makwana <makwanadhruva712@gmail.com>
2025-01-10 08:56:21 +0000

    jsx constant to svg

---
+++ portfolio/src/app/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/components.css'
+const navbar = () => {
+   <>
+    <div className="nav">
+        
+    </div>
+    <div className="Navbar">
+         
+    </div>
+   </>
+}
+export default navbar;
